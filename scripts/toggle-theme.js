38407d02fd68c1baa49df0fcb3e39c33b92896ff
Safalm74 <safalm74@gmail.com>
2024-06-02 18:25:41 +0545

    themes add

--- scripts/toggle-theme.js
+++ scripts/toggle-theme.js
@@ -1,7 +1,7 @@
-var is_day=true;
-const themeToogleBtn=document.getElementById('btn__theme-toggle');
-
+ const themeToogleBtn=document.getElementById('btn__theme-toggle');
+ const body_var = document.querySelector('body')
 
 themeToogleBtn.onclick =function (){
-    console.log('theme clicked');
+    document.body.classList.toggle("night-theme"); 
+    body_var.classList.toggle('body-theme-night');
 }
