93d8dd859d2ea9c32a3e44ef10b5498cf0c77e87
Safalm74 <safalm74@gmail.com>
2024-06-02 20:47:54 +0545

    socialinks style

--- scripts/toggle-theme.js
+++ scripts/toggle-theme.js
@@ -1,7 +1,71 @@
- const themeToogleBtn=document.getElementById('btn__theme-toggle');
- const body_var = document.querySelector('body')
+ const themeToogleBtn=document.getElementsByClassName('header__theme-toggle');
+ const body_var = document.querySelector('body');
+ const social_links=document.getElementsByClassName('ul-social-links');
+ var is_day=true;
 
-themeToogleBtn.onclick =function (){
-    document.body.classList.toggle("night-theme"); 
-    body_var.classList.toggle('body-theme-night');
-}
+ function theme_toggle(is_day){
+    if (is_day){
+        console.log('ThemeChanged: Night');
+        Array.from(social_links).forEach((ul)=>{
+            ul.innerHTML=`
+            <li> 
+                <figure class="square-icon">
+                    <img src="assets/img/night-social-links1.svg" class="icon-img">
+                </figure>
+            </li>
+            <li> 
+                <figure class="square-icon">
+                    <img src="assets/img/night-social-links4.svg" class="icon-img">
+                </figure>
+            </li>
+            <li> 
+                <figure class="square-icon">
+                    <img src="assets/img/night-social-links2.svg" class="icon-img">
+                </figure>
+            </li>
+            <li> 
+                <figure class="square-icon">
+                    <img src="assets/img/night-social-links3.svg" class="icon-img">
+                </figure>
+            </li>
+             `
+        });
+    }
+    else{
+        console.log('ThemeChanged: Day');
+
+        Array.from(social_links).forEach((ul)=>{
+            ul.innerHTML=`
+            <li> 
+                <figure class="square-icon">
+                    <img src="assets/img/social-links1.svg" class="icon-img">
+                </figure>
+            </li>
+            <li> 
+                <figure class="square-icon">
+                    <img src="assets/img/social-links2.svg" class="icon-img">
+                </figure>
+            </li>
+            <li> 
+                <figure class="square-icon">
+                    <img src="assets/img/social-links4.svg" class="icon-img">
+                </figure>
+            </li>
+            <li> 
+                <figure class="square-icon">
+                    <img src="assets/img/social-links3.svg" class="icon-img">
+                </figure>
+            </li>
+            `
+        });
+    }
+ }
+
+Array.from(themeToogleBtn).forEach((btn)=>{
+    btn.onclick =function (){
+        document.body.classList.toggle("night-theme"); 
+        body_var.classList.toggle('body-theme-night');
+        theme_toggle(is_day)
+        is_day?btn.innerHTML='🌝':btn.innerHTML='🌚';
+        is_day?is_day=false:is_day=true;
+}});
